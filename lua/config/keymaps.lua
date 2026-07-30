9eeba25952293726ac9da2e50c262ea75e3cba2d
ZoroGH <zoro.ghc@gmail.com>
2026-07-30 11:51:17 +0800

    add <S-Del> ignore map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here-- Insert mode: jj -> Esc
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode" })
+-- Shift + Backspace：按普通 Backspace 处理
+vim.keymap.set("i", "<S-Del>", "<BS>", {
+    remap = false,
+    desc = "Shift Backspace",
+})
+
+-- 命令行模式也生效
+vim.keymap.set("c", "<S-Del>", "<BS>", {
+    remap = false,
+    desc = "Shift Backspace",
+})
